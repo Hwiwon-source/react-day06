ac522b32e283e7ec64c16059beecc38cc06fce84
Hwiwon Kang <syaron9364@gmail.com>
2024-08-18 00:25:08 +0900

    final commit

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -14,14 +14,20 @@ const WeatherApp = () => {
   // => useEffect 의존성 배열에 함수를 추가하기 위해서는
   //    useCallback으로 감싸서 컴포넌트가 렌더링될 때마다 동일한 함수 참조를 유지하게 함(무분별한 리렌더링 방지)
   // 결론) useEffect hook의 의존성 배열에는 useCallback으로 감싸진 함수를 추가해야만한다..?
+  // => No, useEffect 의존성 배열에는 state, props, 함수 등 여러 값을 넣을 수 있지만,
+  //        함수의 경우에는 동일한 참조를 유지하기 위해 자주 useCallback을 사용한다.
+  //        동일한 참조를 유지해야만 useEffect가 무분별하게 재실행되는 것을 방지한다.
+  //        일단 여기까지 늬앙스만 이해하고 추후 useCallback 숙지한 후 다시 프로젝트 연습할 것.
 
-  // useEffect 훅의 의존성 배열에 추가하기 위해 useCallback사용
-  const getWeatherByCurrentLocation = useCallback(async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${Api_key}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    setWeather(data);
-  }, []);
+  const getWeatherByCurrentLocation = useCallback(
+    async (lat, lon) => {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${Api_key}`;
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+    },
+    [Api_key]
+  );
 
   // useEffect 훅의 의존성 배열에 추가하기 위해 useCallback사용
   const getCurrentLocation = useCallback(() => {
